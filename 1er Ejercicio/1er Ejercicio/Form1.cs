ce88b85418ed0b3139f2b1bb88f22d436f0efed8
Eoleidy-coder <promocion842@gmail.com>
2026-02-11 09:47:14 -0400

    Agregar archivos de proyecto.

---
+++ 1er Ejercicio/1er Ejercicio/Form1.cs
@@ -0,0 +1,15 @@
+namespace _1er_Ejercicio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nnn
+        }
+    }
+}
